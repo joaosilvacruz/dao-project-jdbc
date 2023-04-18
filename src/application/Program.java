69c5a8e0b35a9250b04fb7707482c16c2e35dfe5
joaosilvacruz <joao.cruz.brasil@gmail.com>
2023-04-18 08:37:25 -0300

    created: Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department d1 = new Department(1, "Books");
-        System.out.println(d1);
 
+        LocalDate date = LocalDate.now();
+        Seller s1 = new Seller(3, "Jv", "Jv@gmail.com", date, 3000.00, d1);
+
+        System.out.println(d1);
+        System.out.println(s1);
     }
 }
