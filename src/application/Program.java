5e01df3b34abf55ac645c8992b81709a53c6da2c
joaosilvacruz <joao.cruz.brasil@gmail.com>
2023-04-19 11:19:08 -0300

    Created: Interfaces Dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,6 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
 
 public class Program {
     public static void main(String[] args) {
