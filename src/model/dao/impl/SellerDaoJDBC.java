43ad534b082a99be0a4ac9379a636d7a138e1957
joaosilvacruz <joao.cruz.brasil@gmail.com>
2023-05-05 17:03:13 -0300

    Created: DepartmentDaoJDBC implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -94,6 +94,7 @@ public class SellerDaoJDBC implements SellerDao {
                     "DELETE FROM seller WHERE Id = ?");
             st.setInt(1, id);
             st.executeUpdate();
+
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
         } finally {
